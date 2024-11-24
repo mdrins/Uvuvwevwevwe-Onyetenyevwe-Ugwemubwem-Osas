6467ae1de49e0538a43e2f60ac8ad1f237e643ec
mdrins <100126653+mdrins@users.noreply.github.com>
2024-11-25 00:20:01 +0200

    Update 5Uzd.py

--- 5Uzd.py
+++ 5Uzd.py
@@ -3,6 +3,6 @@ raw_text = "@John: Šis ir lielisks produkts!!! Vai ne? 👏 👏 👏 http://ex
 
 # Remove mentions, URLs, emojis, and special characters
 clean_text = re.sub(r"@\w+|http\S+|[^\w\s]", "", raw_text)
-  clean_text = clean_text.lower()
+clean_text = clean_text.lower()
 print("Tīrs teksts:")
 print(clean_text)
